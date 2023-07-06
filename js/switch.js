25c5a2e25326f4ac00f849bad7a301cecdd75484
Can Kolho <can.kolho@hotmail.com>
2023-07-06 19:30:42 +0300

    home-section (responsive)

--- js/switch.js
+++ js/switch.js
@@ -1,6 +1,27 @@
 const switchInput = document.getElementById('toggle__switch');
-const body = document.body;
+const home = document.getElementById('home');
+const homeBtn = document.getElementById('btn');
+
+function lightmode() {
+  home.style.backgroundColor = 'var(--lightmode-bg2)';
+  home.style.color = 'var(--text-black)';
+  homeBtn.style.color = 'var(--text-black)';
+}
+
+function darkmode() {
+  home.style.backgroundColor = 'var(--darkmode-bg1)';
+  home.style.color = 'var(--text-white)';
+  homeBtn.style.color = 'var(--text-white)';
+};
+
+//darkmode is 'default' when entering page. 
+function check() {
+  document.getElementById("toggle__switch").checked = true;
+  darkmode();
+}
 
 switchInput.addEventListener('click', function () {
-  (switchInput.checked) ? body.style.backgroundColor = '#0D1117' :body.style.backgroundColor = '#FFFFFF'; 
-  });
+  (switchInput.checked) ? darkmode() :lightmode();
+});
+
+check();
