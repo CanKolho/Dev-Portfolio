18b0b2d1c6df628c14c133629641234dd073a6a3
Can Kolho <can.kolho@hotmail.com>
2023-07-08 00:27:27 +0300

    footer animation

--- js/switch.js
+++ js/switch.js
@@ -1,5 +1,6 @@
 const switchInput = document.getElementById('toggle__switch');
 
+const body = document.body;
 const home = document.getElementById('home');
 const homeBtn = document.getElementById('btn');
 const about = document.getElementById('about');
@@ -9,6 +10,9 @@ const projects = document.getElementById('projects');
 const projectHeader = document.getElementById('project-header');
 
 function lightmode() {
+
+  body.style.backgroundColor = 'var(--lightmode-bg1)';
+
   home.style.backgroundColor = 'var(--lightmode-bg2)';
   home.style.color = 'var(--text-black)';
   homeBtn.style.color = 'var(--text-black)';
@@ -27,6 +31,9 @@ function lightmode() {
 }
 
 function darkmode() {
+
+  body.style.backgroundColor = 'var(--darkmode-bg2)';
+
   home.style.backgroundColor = 'var(--darkmode-bg1)';
   home.style.color = 'var(--text-white)';
   homeBtn.style.color = 'var(--text-white)';
