2c734c7664f63cabdefc6df807ac6a9e4a4b8a51
Can Kolho <can.kolho@hotmail.com>
2023-07-07 14:53:31 +0300

    aboutMe-section

--- js/switch.js
+++ js/switch.js
@@ -1,17 +1,26 @@
 const switchInput = document.getElementById('toggle__switch');
+
 const home = document.getElementById('home');
 const homeBtn = document.getElementById('btn');
+
+const about = document.getElementById('about');
 
 function lightmode() {
   home.style.backgroundColor = 'var(--lightmode-bg2)';
   home.style.color = 'var(--text-black)';
   homeBtn.style.color = 'var(--text-black)';
+
+  about.style.backgroundColor = 'var(--lightmode-bg1)';
+  about.style.color =  'var(--text-black)';
 }
 
 function darkmode() {
   home.style.backgroundColor = 'var(--darkmode-bg1)';
   home.style.color = 'var(--text-white)';
   homeBtn.style.color = 'var(--text-white)';
+
+  about.style.backgroundColor = 'var(--darkmode-bg2)';
+  about.style.color =  'var(--text-white)';
 };
 
 //darkmode is 'default' when entering page. 
